5f6ec489104dfb1f1f92b45dbed77ee366e0de18
Justin Carver <15Jcarver202@gmail.com>
2016-10-07 09:44:57 -0600

    Initial commit

---
+++ Randomizer/Randomizer/People Controller.swift
@@ -0,0 +1,9 @@
+//
+//  People Controller.swift
+//  Randomizer
+//
+//  Created by Justin Carver on 10/7/16.
+//  Copyright © 2016 Justin Carver. All rights reserved.
+//
+
+import Foundation
---
+++ Randomizer/Randomizer/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Randomizer
+//
+//  Created by Justin Carver on 10/7/16.
+//  Copyright © 2016 Justin Carver. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    
+}
